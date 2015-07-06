bca059d33e3ab36040c31fb83e4d0397c54f8c4b
wilsoncampusano <wilsoncampusanojorge@gmail.com>
2015-07-06 16:32:45 -0400

    Environment: Nunit

---
+++ airplaneSeat.Specs/Environment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace airplaneSeat.Specs
+{
+    [TestFixture]
+    class Environment
+    {
+
+        [Test]
+        public void Fails()
+        {
+            Assert.Fail("");
+        }
+    }
+}
